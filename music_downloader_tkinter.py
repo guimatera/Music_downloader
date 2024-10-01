0eb207a3adc4941b3dcde6ea6015c9fefa09dd9e
Guilherme Matera <guimatera@gmail.com>
2024-10-01 17:30:58 -0300

    Feature: Changing script interface from pysimpleGUI to tkinter

--- music_downloader_tkinter.py
+++ music_downloader_tkinter.py
@@ -1,13 +1,18 @@
 import os
+from mutagen import File
 from pytube import YouTube
 import tkinter as tk
-from tkinter import messagebox, ttk
+from tkinter import messagebox
+import glob
+from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
 
 class MusicDownloadApp:
     def __init__(self, root):
         self.root = root
         self.root.title('Music Downloader')
-        self.root.geometry('400x200')
+        self.root.geometry('800x200')
+        self.pady = 5
 
         # Initialize radio button variable
         self.radio_var = tk.StringVar(value='mp3')
@@ -18,22 +23,22 @@ class MusicDownloadApp:
 
         # Layout Widgets
         self.label = tk.Label(frame, text='Download YouTube:', font='Consolas')
-        self.label.grid(row=0, column=0, padx=5, pady=5)
+        self.label.grid(row=0, column=0, padx=5, pady=self.pady, sticky='w')
 
         self.link_label = tk.Label(frame, text='Link:', font='Consolas')
-        self.link_label.grid(row=1, column=0, padx=5, pady=5)
+        self.link_label.grid(row=1, column=0, padx=5, pady=self.pady, sticky='w')
 
-        self.link_entry = tk.Entry(frame, width=30, font='Consolas')
-        self.link_entry.grid(row=1, column=1, padx=0, pady=0, sticky='w')
+        self.link_entry = tk.Entry(frame, width=60, font='Consolas')
+        self.link_entry.grid(row=1, column=0, padx=100, pady=self.pady, sticky='w')
 
         self.mp3_radio = tk.Radiobutton(frame, text='Convert to MP3', value='mp3', variable=self.radio_var, font='Consolas')
-        self.mp3_radio.grid(row=2, column=0, padx=0, pady=0, sticky='w')
+        self.mp3_radio.grid(row=2, column=0, padx=5, pady=self.pady, sticky='w')
 
         self.mp4_radio = tk.Radiobutton(frame, text='Convert to MP4', value='mp4', variable=self.radio_var, font='Consolas')
-        self.mp4_radio.grid(row=2, column=1, padx=5, pady=5, sticky='w')
+        self.mp4_radio.grid(row=2, column=0, padx=200, pady=self.pady, sticky='w')
 
         self.download_button = tk.Button(frame, text='Download', command=self.download, font='Consolas')
-        self.download_button.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+        self.download_button.grid(row=3, column=0, padx=5, pady=self.pady)
 
 
 
@@ -48,14 +53,20 @@ class MusicDownloadApp:
                 self.convert_mp4(yt)
             messagebox.showinfo('Success', 'Download successfully completed!')
         except Exception as e:
-            messagebox.showerror('Error', 'Please, enter a valid link!')
+            messagebox.showerror('Error', e)
 
     def convert_mp3(self, yt):
         audio = yt.streams.filter(file_extension='mp4')
         file = audio.first().download('musics')
-        base, extension = os.path.splitext(file)
+        base,extension = os.path.splitext(file)
         new_file = base + '.mp3'
         os.rename(file, new_file)
+        print(new_file)
+
+        #song = File(new_file)
+        #song['artist'] = yt.author
+        #song.save()
+
 
     def convert_mp4(self, yt):
         video = yt.streams.filter(file_extension='mp4').get_highest_resolution()
